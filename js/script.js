53d19b3682c687f8a38a4f6e1af3ae9b4b49c6a8
kodego teacher <kodegoteacher@gmail.com>
2023-08-17 02:43:22 -0700

    TV Show Details

--- js/script.js
+++ js/script.js
@@ -121,6 +121,60 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 }
 
+//Display TV Show Details
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+
+  const show = await fetchAPIData(`tv/${showId}`); 
+
+  //Overlay for background image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement('div')
+
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.title}"/>` 
+  : `      <img
+  src="./images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+/>`}
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(2)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+     ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${show.homepage}" target="_blank" class="btn">Visit TV Show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>TV Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+    <li><span class="text-secondary">Last Episode to Air:</span> ${show.last_episode_to_air}</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${show.production_companies.map((company) => `<span>${company.name} </span>`).join(", ")}</div>
+</div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
+}
+
+
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
 const API_KEY = '2c7b41aeee968eb46df10ecb0df3793c';
@@ -188,6 +242,7 @@ function addCommasToNumber(number) {
 function init() {
   switch(global.currentPage) {
     case "/":
+    case "/index.html":
       displayPopularMovies();
       break;
     case "/shows.html":
@@ -197,7 +252,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
