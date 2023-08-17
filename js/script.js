e45be53c56623e0e246981c55edd3edb838a861c
kodego teacher <kodegoteacher@gmail.com>
2023-08-16 23:39:44 -0700

    Popular TV Shows Fetching and displaying information

--- js/script.js
+++ js/script.js
@@ -35,6 +35,37 @@ async function displayPopularMovies() {
   })
 }
 
+//Displaying Popular TV Shows
+async function displayPopularShows() {
+  const {results} = await fetchAPIData('/tv/popular')
+ 
+  results.forEach((show) => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    
+    
+    <a href="tv-details.html?id=${show.id}">
+    ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}"/>` 
+    : `      <img
+    src="./images/no-image.jpg"
+    class="card-img-top"
+    alt="Show Title"
+  />`}
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${show.first_air_date}</small>
+      </p>
+    </div>
+  
+    `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  })
+}
+
 
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
@@ -83,7 +114,7 @@ function init() {
       displayPopularMovies();
       break;
     case "/shows.html":
-      console.log("Shows");
+      displayPopularShows();
       break;
     case "/movie-details.html":
       console.log("Movie Details");
